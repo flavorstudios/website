f552282808732f8c7ae77d95ececacdebb3a01de
Flavor Studios <balgatha77@gmail.com>
2025-05-17 14:02:21 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "flavor-studios-v1";
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/screenshots/homepage.png",
+  "/screenshots/blog.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});

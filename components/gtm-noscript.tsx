434bc321a9eee28584480a090d3066b85555752d
Flavor Studios <balgatha77@gmail.com>
2025-05-21 15:40:33 +0530

    Update gtm-noscript.tsx

--- components/gtm-noscript.tsx
+++ components/gtm-noscript.tsx
@@ -1,16 +1,17 @@
 export default function GTMNoScript() {
   return (
-    <>
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-W7GC5SVZ"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        />
-      </noscript>
-      {/* End Google Tag Manager (noscript) */}
-    </>
-  )
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <!-- Google Tag Manager (noscript) -->
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7GC5SVZ"
+              height="0" width="0" style="display:none;visibility:hidden">
+            </iframe>
+          </noscript>
+          <!-- End Google Tag Manager (noscript) -->
+        `,
+      }}
+    />
+  );
 }

7dc9b2d1d93c4e07575ea33499de0f9802ca5fa0
Flavor Studios <balgatha77@gmail.com>
2025-06-24 14:39:56 +0530

    Create head.tsx

---
+++ app/disclaimer/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebPage",
+            "name": "Disclaimer",
+            "url": "https://flavorstudios.in/disclaimer",
+            "description":
+              "Read Flavor Studios' official disclaimer outlining legal limitations, liability, third-party links, and content usage policies.",
+            "publisher": {
+              "@type": "Organization",
+              "name": "Flavor Studios",
+              "url": "https://flavorstudios.in",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "https://flavorstudios.in/logo.png",
+              },
+            },
+          }),
+        }}
+      />
+    </>
+  );
+}

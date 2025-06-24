927aa4fa74bf273a2ff94ad7e010dd35d2b943d5
Flavor Studios <balgatha77@gmail.com>
2025-06-24 12:09:08 +0530

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -27,10 +27,49 @@ export default function Head() {
 
   return (
     <>
+      {/* === Favicon === */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+
+      {/* === PWA Manifest & Theme === */}
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <meta name="theme-color" content="#000000" />
+
+      {/* === Apple PWA Support === */}
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+      {/* === Facebook Open Graph App ID === */}
+      <meta property="fb:app_id" content="1404440770881914" />
+
+      {/* === Mastodon Verification === */}
+      <link rel="me" href="https://mastodon.social/@flavorstudios" />
+
+      {/* === Open Graph Site Name (fixes og:site_name not provided) === */}
+      <meta property="og:site_name" content="Flavor Studios" />
+
+      {/* === JSON-LD Schema === */}
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(schema)
+        }}
       />
+
+      {/* === Google Tag Manager (HEAD) === */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMTGR7NM');
+          `,
+        }}
+      />
+      {/* === END GTM (HEAD) === */}
     </>
   );
 }

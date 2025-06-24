c5b1e10a0fd5236d1556746766aa8ae39f9187c6
Flavor Studios <balgatha77@gmail.com>
2025-06-24 14:13:04 +0530

    Create head.tsx

---
+++ app/admin/dashboard/head.tsx
@@ -0,0 +1,40 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebApplication",
+            "name": "Flavor Studios Admin Dashboard",
+            "description":
+              "Access all admin tools to manage posts, videos, comments, and more for Flavor Studios from a single secure dashboard.",
+            "url": "https://flavorstudios.in/admin/dashboard",
+            "applicationCategory": "AdministrativeApplication",
+            "publisher": {
+              "@type": "Organization",
+              "name": "Flavor Studios",
+              "url": "https://flavorstudios.in",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "https://flavorstudios.in/logo.png",
+              },
+              "sameAs": [
+                "https://www.youtube.com/@flavorstudios",
+                "https://www.instagram.com/flavorstudios",
+                "https://twitter.com/flavor_studios",
+                "https://www.facebook.com/flavourstudios",
+                "https://www.threads.net/@flavorstudios",
+                "https://discord.com/channels/@flavorstudios",
+                "https://t.me/flavorstudios",
+                "https://www.reddit.com/r/flavorstudios/",
+                "https://bsky.app/profile/flavorstudios.bsky.social"
+              ]
+            }
+          }),
+        }}
+      />
+    </>
+  );
+}

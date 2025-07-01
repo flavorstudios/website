b6daab25ddfe0e4f7e5138b41c41e188f71a9cb9
Flavor Studios <balgatha77@gmail.com>
2025-07-01 19:32:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  swSrc: 'app/sw.js', // This is your source template!
+  swSrc: 'app/sw.js', // This is your service worker source template!
   disable: process.env.NODE_ENV === 'development',
+  // 💡 This guarantees /offline is ALWAYS precached (so offline fallback never fails)
+  additionalManifestEntries: [
+    { url: '/offline', revision: null },
+  ],
 });
 
 const nextConfig = {

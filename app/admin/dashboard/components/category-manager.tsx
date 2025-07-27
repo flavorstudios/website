a7677e11bf65ce9527e99496622eb201cd3a2a40
Flavor Studios <balgatha77@gmail.com>
2025-07-28 02:09:37 +0530

    Update category-manager.tsx

--- app/admin/dashboard/components/category-manager.tsx
+++ app/admin/dashboard/components/category-manager.tsx
@@ -1,4 +1,8 @@
-"use client"
+"use client";
 
-export { default as CategoryManager } from "@/components/admin/category/CategoryList"
-export type { CategoryListProps, Category, CategoryType } from "@/components/admin/category/CategoryList"
+export { default as CategoryManager } from "@/components/admin/category/CategoryManager";
+export type {
+  CategoryListProps,
+  Category,
+  CategoryType,
+} from "@/components/admin/category/CategoryList";

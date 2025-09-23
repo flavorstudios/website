301239d5c12e309f3d008d4b6950c89dc89c3c9c
Flavor Studios <balgatha77@gmail.com>
2025-09-23 17:52:20 +0530

    Your commit message

--- app/admin/dashboard/components/header/user-menu.tsx
+++ app/admin/dashboard/components/header/user-menu.tsx
@@ -73,7 +73,7 @@ export function UserMenu({ avatar, name, userRole, onLogout }: UserMenuProps) {
           onSelect={onLogout}
           className="text-destructive focus:text-destructive"
         >
-          <LogOut className="h-4 w-4 mr-2" /> Log out
+          <LogOut className="h-4 w-4 mr-2" /> Sign out
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>

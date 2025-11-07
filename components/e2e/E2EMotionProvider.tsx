dcbe134951bdab3036673244c7c0722fb75696d9
Flavor Studios <balgatha77@gmail.com>
2025-11-07 15:46:42 +0530

    Your commit message

--- components/e2e/E2EMotionProvider.tsx
+++ components/e2e/E2EMotionProvider.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren, ReactElement } from "react";
 import { MotionConfig } from "framer-motion";
 
 import { isClientE2EEnabled } from "@/lib/e2e-utils";
 
-export function E2EMotionProvider({ children }: PropsWithChildren): JSX.Element {
+export function E2EMotionProvider({ children }: PropsWithChildren): ReactElement {
   const reduceMotion = isClientE2EEnabled();
 
   if (!reduceMotion) {
